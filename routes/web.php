636a933b3ed917a718e143c942c4f8a8789cb1c6
AhmadAlketnani <ahmadalktnany383@gmail.com>
2023-09-24 01:32:20 +0300

    upload laravel login register system

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('register', 'AuthController@register')->name('register');
+    Route::post('register', 'AuthController@registerPost')->name('register');
+
+    Route::get('login', 'AuthController@login')->name('login');
+    Route::post('login', 'AuthController@loginPost')->name('login');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('home', 'HomeController@index');
+    Route::delete('logout','AuthController@logout')->name('logout');
+});
